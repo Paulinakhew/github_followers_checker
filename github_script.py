9fe20bf23a4e4cdf9ff8fd5d7232326c21514b0d
Paulina Khew <paulinakhew12345@hotmail.com>
2020-05-31 21:30:54 -0400

    :tada: Initial commit of script

---
+++ github_script.py
@@ -0,0 +1,21 @@
+import requests
+
+MAX_ENTRIES_PER_PAGE = 75
+
+followers = requests.get(f'https://api.github.com/users/paulinakhew/followers?per_page={MAX_ENTRIES_PER_PAGE}').json()
+following = requests.get(f'https://api.github.com/users/paulinakhew/following?per_page={MAX_ENTRIES_PER_PAGE}').json()
+
+followers_usernames = []
+following_usernames = []
+
+for person in followers:
+    followers_usernames.append(person['login'])
+
+for person in following:
+    following_usernames.append(person['login'])
+
+negative_followers = [x for x in following_usernames if x not in followers_usernames]
+extra_followers = [x for x in followers_usernames if x not in following_usernames]
+
+print("people that don't follow me back", negative_followers, len(negative_followers))
+print("people I don't follow back", extra_followers, len(extra_followers))
