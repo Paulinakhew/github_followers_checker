0ceba35ecb2b7fdd6e97e43f065a60842154a1af
Paulina Khew <paulinakhew12345@hotmail.com>
2020-06-02 17:24:09 -0400

    :art: Move code into separate functions

--- github_script.py
+++ github_script.py
@@ -4,46 +4,88 @@ import requests
 from dotenv import load_dotenv
 
 MAX_ENTRIES_PER_PAGE = 100
-USER_TO_FOLLOW = ''  # hardcoded for now
 
 load_dotenv()
 MY_TOKEN = os.environ.get("MY_TOKEN")
 
 headers = {'Authorization': 'token ' + MY_TOKEN}
 
-followers = requests.get(
-    f'https://api.github.com/users/paulinakhew/followers?per_page={MAX_ENTRIES_PER_PAGE}',
-    headers=headers
-).json()
 
-following = requests.get(
-    f'https://api.github.com/users/paulinakhew/following?per_page={MAX_ENTRIES_PER_PAGE}',
-    headers=headers
-).json()
+def get_all_followers():
+    followers = requests.get(
+        f'https://api.github.com/users/paulinakhew/followers?per_page={MAX_ENTRIES_PER_PAGE}',
+        headers=headers
+    ).json()
+    return followers
 
-followers_usernames = []
-following_usernames = []
 
-for person in followers:
-    followers_usernames.append(person['login'])
+def get_all_following():
+    following = requests.get(
+        f'https://api.github.com/users/paulinakhew/following?per_page={MAX_ENTRIES_PER_PAGE}',
+        headers=headers
+    ).json()
+    return following
 
-for person in following:
-    following_usernames.append(person['login'])
 
-negative_followers = [x for x in following_usernames if x not in followers_usernames]
-extra_followers = [x for x in followers_usernames if x not in following_usernames]
+def get_follower_groups(followers: list, following: list):
+    followers_usernames = []
+    following_usernames = []
 
-print("people that don't follow me back", negative_followers, len(negative_followers))
-print("people I don't follow back", extra_followers, len(extra_followers))
+    for person in followers:
+        followers_usernames.append(person['login'])
 
-data = {
-    "Content-Length": 0
-}
+    for person in following:
+        following_usernames.append(person['login'])
 
-# unfollow users
-resp = requests.put(
-    url=f'https://api.github.com/user/following/{USER_TO_FOLLOW}',
-    data=json.dumps(data),
-    headers=headers
-)
-print(resp.json())
+    negative_followers = [x for x in following_usernames if x not in followers_usernames]
+    extra_followers = [x for x in followers_usernames if x not in following_usernames]
+
+    print("people that don't follow me back", negative_followers, len(negative_followers))
+    print("people I don't follow back", extra_followers, len(extra_followers))
+    return negative_followers, extra_followers
+
+
+def hit_unfollow_endpoint(username, headers):
+    data = {"Content-Length": 0}
+
+    resp = requests.put(
+        url=f'https://api.github.com/user/following/{username}',
+        data=json.dumps(data),
+        headers=headers
+    )
+    return resp
+
+
+def hit_follow_endpoint(username, headers):
+    resp = requests.delete(
+        url=f'https://api.github.com/user/following/{username}',
+        headers=headers
+    )
+    return resp
+
+
+def unfollow_all(negative_followers):
+    for username in negative_followers:
+        resp = hit_unfollow_endpoint(username, headers)
+        if resp.status_code == 204:
+            print(f'{username} unfollowed successfully')
+        else:
+            print(f'Could not unfollow {username}', resp.status_code)
+
+
+def follow_all(extra_followers):
+    for username in extra_followers:
+        resp = hit_follow_endpoint(username, headers)
+        if resp.status_code == 204:
+            print(f'{username} followed successfully')
+        else:
+            print(f'Could not unfollow {username}', resp.status_code)
+
+
+if __name__ == '__main__':
+    followers = get_all_followers()
+    following = get_all_following()
+
+    negative_followers, extra_followers = get_follower_groups(followers, following)
+    unfollow_all(negative_followers)
+    follow_all(extra_followers)
